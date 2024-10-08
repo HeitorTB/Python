93afc6f12ad1ba79368d17268aa0e5fc44d31744
Heitor Brasil <heitortbr@gmail.com>
2024-10-08 15:59:56 -0300

    Projeto pyautogui
    Projeto aula 1 finalizado

--- Projeto_analise/python.py
+++ Projeto_analise/python.py
@@ -2,7 +2,7 @@ import pyautogui
 import time 
 import pandas 
 
-pyautogui.PAUSE= 0.7
+pyautogui.PAUSE= 1.0
 
 pyautogui.press("win")
 pyautogui.write("chrome")
@@ -18,5 +18,41 @@ pyautogui.click(x=526, y=471)
 pyautogui.write("heitortbr1234")
 pyautogui.click(x=639, y=546)
 
-tabela = pandas.read_csv("")
+tabela = pandas.read_csv("produtos.csv")
 print(tabela)
+
+linha=0
+for linha in tabela.index:
+    pyautogui.click(x=545, y=260)
+    
+    codigo= tabela.loc[linha,"codigo"]
+    pyautogui.write(str(codigo))
+    pyautogui.press("tab")
+
+    marca= tabela.loc[linha,"marca"]
+    pyautogui.write(str(marca))
+    pyautogui.press("tab")
+
+    tipo= tabela.loc[linha,"tipo"]
+    pyautogui.write(str(tipo))
+    pyautogui.press("tab")
+
+    categoria = tabela.loc[linha, "categoria"]
+    pyautogui.write(str(categoria))
+    pyautogui.press("tab")
+
+    preço = tabela.loc[linha, "preco_unitario"]
+    pyautogui.write(str(preço))
+    pyautogui.press("tab")
+
+    custo = tabela.loc[linha, "custo"]
+    pyautogui.write(str(custo))
+    pyautogui.press("tab")
+
+    obs= tabela.loc[linha, "obs"]
+    if not pandas.isna(obs):
+        pyautogui.write(str(obs))
+    
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.scroll("10000")
